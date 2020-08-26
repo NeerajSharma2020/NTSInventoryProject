197cbd18b5204e6ddff95ab008755c849b90768a
NeerajSharma2020 <neeraj.sharma@successive.tech>
2020-08-26 15:16:10 +0530

    change in OrderDetails save mutation as per frontend request.

--- src/main/java/com/st/ims/resolvers/OrderDetailsMutationResolver.java
+++ src/main/java/com/st/ims/resolvers/OrderDetailsMutationResolver.java
@@ -1,5 +1,7 @@
 package com.st.ims.resolvers;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -9,58 +11,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.st.ims.model.OrderDetails;
+import com.st.ims.model.OrderProducts;
 import com.st.ims.repo.OrderDetailsRepository;
-import com.st.ims.repo.OrderRepository;
-import com.st.ims.repo.ProductRepository;
 import com.st.ims.utility.AppUtility;
 
 import graphql.kickstart.tools.GraphQLMutationResolver;
 
 @Component
 @Transactional
-public class OrderDetailsMutationResolver implements GraphQLMutationResolver{
-	
+public class OrderDetailsMutationResolver implements GraphQLMutationResolver {
+
 	private final Logger logger = Logger.getLogger(this.getClass());
 
 	@Autowired
 	private OrderDetailsRepository orderDetailsRepo;
-	
-	@Autowired
-	private ProductRepository productRepo;
-	
-	@Autowired
-	private OrderRepository orderRepo;
-	
+
 	@PersistenceContext
 	private EntityManager entityManager;
-	
+
 	/*
 	 * This method will take orderDetails object and productId, it will persist
 	 * orderDetails object into database and make relationship with product with
 	 * passed productId.
 	 */
-	public OrderDetails saveOrderDetails(OrderDetails orderDetail, int productId) {
+	public List<OrderDetails> saveOrderDetails(OrderDetails orderDetail, List<OrderProducts> orderProductList) {
+		List<OrderDetails> orderDetailsList = null;
 		try {
-			OrderDetails filledOrderDetails = AppUtility.setDefaultValues(orderDetail);
-			filledOrderDetails.setProduct(productRepo.findById(productId).orElse(null));
-			logger.info("initial order Id....."+orderDetail.getOrder().getOrderId());
-			if(orderDetail.getOrder().getOrderId() == 0) {
-				orderRepo.save(orderDetail.getOrder());
+			orderDetailsList = AppUtility.getOrderDetailsList(orderDetail, orderProductList);
+			if (orderDetail.getOrder() != null) {
+				entityManager.persist(orderDetail.getOrder());
 			}
-			return orderDetailsRepo.save(filledOrderDetails);
+			return orderDetailsRepo.saveAll(orderDetailsList);
 		} catch (Exception e) {
-			logger.error("Exception while saving Order Details.",e);
+			logger.error("Exception while saving Order Details.", e);
 		}
-		return orderDetail;
+		return orderDetailsList;
 	}
 
-	/* This method will take orderDetailsId as integer and will delete OrderDetails with it.*/
+	/*
+	 * This method will take orderDetailsId as integer and will delete OrderDetails
+	 * with it.
+	 */
 	public boolean deleteOrderDetailById(int orderDetailsId) {
 		try {
 			orderDetailsRepo.deleteById(orderDetailsId);
 			return true;
 		} catch (Exception e) {
-			logger.error("Exception while deleting Order Details.",e);
+			logger.error("Exception while deleting Order Details.", e);
 			return false;
 		}
 
@@ -69,17 +66,16 @@ public class OrderDetailsMutationResolver implements GraphQLMutationResolver{
 	/* This method will take OrderDetails Object and return new updated Object. */
 	public OrderDetails updateOrderDetails(OrderDetails orderDetail) {
 		try {
-			if(orderDetailsRepo.existsById(orderDetail.getOrderDetailId())) {
+			if (orderDetailsRepo.existsById(orderDetail.getOrderDetailId())) {
 				entityManager.merge(orderDetail.getOrder());
 				return entityManager.merge(orderDetail);
-			}else {
+			} else {
 				logger.error("OrderDetails Id you are looking for doesn't exist.");
 			}
 		} catch (Exception e) {
-			logger.error("Exception while updaing Invoice Details.",e);
+			logger.error("Exception while updaing Invoice Details.", e);
 		}
 		return null;
 	}
 
-	
 }
--- src/main/java/com/st/ims/utility/AppUtility.java
+++ src/main/java/com/st/ims/utility/AppUtility.java
@@ -3,6 +3,8 @@ package com.st.ims.utility;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.apache.log4j.Logger;
@@ -12,84 +14,101 @@ import com.st.ims.model.Invoice;
 import com.st.ims.model.InvoiceDetails;
 import com.st.ims.model.Order;
 import com.st.ims.model.OrderDetails;
+import com.st.ims.model.OrderProducts;
 
 @Service
 public class AppUtility {
-	
+
 	private static final Logger logger = Logger.getLogger(AppUtility.class);
-	
+
 	private AppUtility() {
-		
+
 	}
+
 	/*
-	 * This method will take LocalDateTime as date and will return after two
-	 * months date.
+	 * This method will take LocalDateTime as date and will return after two months
+	 * date.
 	 */
 	private static LocalDateTime getDueDate(LocalDateTime createdDate) {
-		if(createdDate != null) {
+		if (createdDate != null) {
 			return createdDate.plusMonths(2);
 		}
 		return createdDate;
-	
-		
+
 	}
 
 	/* This method will return alphanumeric order number. */
-	
+
 	private static String getInvoiceNumber() throws NoSuchAlgorithmException {
 		Random rand;
 		int leftLimit = 48; // numeral '0'
 		int rightLimit = 122; // letter 'z'
 		int targetStringLength = 10;
-        rand = SecureRandom.getInstanceStrong();
- 
-		return rand.ints(leftLimit, rightLimit + 1)
-		  .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-		  .limit(targetStringLength)
-		  .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-		  .toString();
-		   
-	   
+		rand = SecureRandom.getInstanceStrong();
+
+		return rand.ints(leftLimit, rightLimit + 1).filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+				.limit(targetStringLength)
+				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+
 	}
-	
+
 	/*
 	 * This method will take order amount and commission percentage and will return
 	 * commission amount.
 	 */
-	private static double getCommissionAmount(double invoiceAmount , double commissionPercentage) {
-		return invoiceAmount*(commissionPercentage/100);
+	private static double getCommissionAmount(double invoiceAmount, double commissionPercentage) {
+		return invoiceAmount * (commissionPercentage / 100);
 	}
-	
-	
+
 	public static InvoiceDetails setDefaultValues(InvoiceDetails invoiceDetail) {
 		try {
 			Invoice invoice = invoiceDetail.getInvoice();
 			invoice.setDueDate(getDueDate(invoice.getCreateDate()));
-			invoice.setCommissionAmount(getCommissionAmount(invoice.getInvoiceAmount(),invoice.getCommissionPercentage()));
+			invoice.setCommissionAmount(
+					getCommissionAmount(invoice.getInvoiceAmount(), invoice.getCommissionPercentage()));
 			invoice.setInvoiceNumber(getInvoiceNumber());
 			invoiceDetail.setInvoice(invoice);
 			return invoiceDetail;
 		} catch (NoSuchAlgorithmException e) {
-			logger.error("Exception while configuring dueDate,invoiceNumber and commissionAmount.",e);
+			logger.error("Exception while configuring dueDate,invoiceNumber and commissionAmount.", e);
 		}
 		return invoiceDetail;
-		
-		
+
 	}
-	
+
 	public static OrderDetails setDefaultValues(OrderDetails orderDetail) {
 		try {
 			Order order = orderDetail.getOrder();
 			order.setDueDate(getDueDate(order.getCreateDate()));
-			order.setCommissionAmount(getCommissionAmount(order.getOrderAmount(),order.getCommissionPercentage()));
+			order.setCommissionAmount(getCommissionAmount(order.getOrderAmount(), order.getCommissionPercentage()));
 			order.setOrderNumber(getInvoiceNumber());
 			orderDetail.setOrder(order);
 			return orderDetail;
 		} catch (NoSuchAlgorithmException e) {
-			logger.error("Exception while configuring dueDate,orderNumber and commissionAmount.",e);
+			logger.error("Exception while configuring dueDate,orderNumber and commissionAmount.", e);
 		}
 		return orderDetail;
-		
-		
+	}
+	
+	public static List<OrderDetails> getOrderDetailsList(OrderDetails ordereDetail, List<OrderProducts> orderProductList){
+		List<OrderDetails> orderDetailsList = new ArrayList<>();
+		try {
+			OrderDetails filledOrderDetails = setDefaultValues(ordereDetail);
+			if(!orderProductList.isEmpty()) {
+				for(OrderProducts productList:orderProductList){
+					OrderDetails newOrderDetail = new OrderDetails();
+					newOrderDetail.setOrder(filledOrderDetails.getOrder());
+					newOrderDetail.setProduct(productList.getProduct());
+					newOrderDetail.setProductCount(productList.getProductQuantity());
+					orderDetailsList.add(newOrderDetail);
+				}
+				return orderDetailsList;
+			}else {
+				logger.error("Order doesn,t have any product associated with it.");
+			}
+		} catch (Exception e) {
+			logger.error("Exception while getting OrderDetailsList.",e);
+		}
+		return orderDetailsList;
 	}
 }
