88b5f4a2fe3be1c8523134b91170a91ece7e3683
NeerajSharma2020 <neeraj.sharma@successive.tech>
2020-08-28 19:33:12 +0530

    order code added.

--- src/main/java/com/st/ims/utility/AppUtility.java
+++ src/main/java/com/st/ims/utility/AppUtility.java
@@ -4,6 +4,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -13,11 +14,15 @@ import org.springframework.stereotype.Service;
 import com.st.ims.model.Invoice;
 import com.st.ims.model.InvoiceDetails;
 import com.st.ims.model.InvoiceProducts;
+import com.st.ims.model.Order;
+import com.st.ims.model.OrderDetails;
+import com.st.ims.model.OrderProducts;
 
 @Service
 public class AppUtility {
 	
 	private static final Logger logger = Logger.getLogger(AppUtility.class);
+	private static final int PERCENTAGE_DENOMINATOR = 100;
 	
 	private AppUtility() {
 		
@@ -58,7 +63,7 @@ public class AppUtility {
 	 * commission amount.
 	 */
 	private static double getCommissionAmount(double invoiceAmount , double commissionPercentage) {
-		return invoiceAmount*(commissionPercentage/100);
+		return invoiceAmount*(commissionPercentage/PERCENTAGE_DENOMINATOR);
 	}
 	
 	/* This method will set default values for invoice object. */
@@ -76,10 +81,11 @@ public class AppUtility {
 
 	/* This method will create a new InvoiceDetails List, so that we can persist invoice object with this list */
 	public static List<InvoiceDetails> getInvoiceDetailsList(Invoice invoice, List<InvoiceProducts> invoiceProductList){
-		List<InvoiceDetails> invoiceDetailsList = new ArrayList<>();
+		List<InvoiceDetails> invoiceDetailsList = null;
 		try {
 			Invoice filledInvoice = setDefaultValues(invoice);
 			if(!invoiceProductList.isEmpty()) {
+				 invoiceDetailsList = new ArrayList<>();
 				for(InvoiceProducts invoiceProduct:invoiceProductList){
 					InvoiceDetails newInvoiceDetail = new InvoiceDetails();
 					newInvoiceDetail.setInvoice(filledInvoice);
@@ -87,34 +93,106 @@ public class AppUtility {
 					newInvoiceDetail.setProductCount(invoiceProduct.getProductQuantity());
 					invoiceDetailsList.add(newInvoiceDetail);
 				}
+				return invoiceDetailsList;
 			}else {
-				logger.error("Invoice doesn,t have any product associated with it.");
+				logger.info("Invoice doesn,t have any product associated with it.");
 			}
-			    return invoiceDetailsList;
+			    return Collections.emptyList();
 		} catch (Exception e) {
 			logger.error("Exception while getting InvoiceDetailsList.",e);
 		}
-		return invoiceDetailsList;
+		return Collections.emptyList();
 	}
 	
 	/* This method will create new InvoiceDetails list for update operation. */
 	public static List<InvoiceDetails> getUpdatedInvoiceList(Invoice invoice,List<InvoiceProducts> invoiceProductList){
 	
-		List<InvoiceDetails> invoiceDetailsList = new ArrayList<>();
+		List<InvoiceDetails> invoiceDetailsList = null;
 		try {
+			if(!invoiceProductList.isEmpty()) {
+				invoiceDetailsList = new ArrayList<>();
 			for(InvoiceProducts invoiceProduct:invoiceProductList) {
 				 InvoiceDetails invoiceDetail = new InvoiceDetails();
 				 invoiceDetail.setInvoice(invoice);
 				 invoiceDetail.setProduct(invoiceProduct.getProduct());
 				 invoiceDetail.setProductCount(invoiceProduct.getProductQuantity());
 				 invoiceDetailsList.add(invoiceDetail);
-				 
-			 }
-			 return invoiceDetailsList;
+				 }
+			return invoiceDetailsList;
+			}else {
+				logger.info("Invoice doesn,t have any product associated with it.");
+			}
+			 return Collections.emptyList();
 		}catch(Exception e) {
 			logger.error("Exception while setting invoice detials list for updation.",e);
 		}
-		return invoiceDetailsList;
+		return Collections.emptyList();
 	}
+	
+	/*
+	 * This method will set default values for order such as orderAmount,
+	 * orderNumber and dueDate.
+	 */
+	public static Order setDefaultValues(Order order) {
+		try {
+			order.setDueDate(getDueDate(order.getCreateDate()));
+			order.setCommissionAmount(getCommissionAmount(order.getOrderAmount(), order.getCommissionPercentage()));
+			order.setOrderNumber(getInvoiceNumber());
+			return order;
+		} catch (NoSuchAlgorithmException e) {
+			logger.error("Exception while configuring dueDate,orderNumber and commissionAmount.", e);
+		}
+		return null;
+	}
+
+	/* This method will return OrderDetails List with products. */
+	public static List<OrderDetails> getOrderDetailsList(Order order, List<OrderProducts> orderProductList) {
+		try {
+			List<OrderDetails> orderDetailsList = null;
+			Order filledOrder = setDefaultValues(order);
+			if (!orderProductList.isEmpty()) {
+				orderDetailsList = new ArrayList<>();
+				for (OrderProducts productList : orderProductList) {
+					OrderDetails newOrderDetail = new OrderDetails();
+					newOrderDetail.setOrder(filledOrder);
+					newOrderDetail.setProduct(productList.getProduct());
+					newOrderDetail.setProductCount(productList.getProductQuantity());
+					orderDetailsList.add(newOrderDetail);
+				}
+				return orderDetailsList;
+			} else {
+				logger.info("Order doesn,t have any product associated with it.");
+			}
+		} catch (Exception e) {
+			logger.error("Exception while getting OrderDetailsList.", e);
+		}
+		return Collections.emptyList();
+	}
+
+	/* This method will create new OrderDetails list for update operation. */
+	public static List<OrderDetails> getUpdatedOrderList(Order order, List<OrderProducts> orderProductList) {
+		try {
+			List<OrderDetails> orderDetailsList = null;
+			if (!orderProductList.isEmpty()) {
+				orderDetailsList = new ArrayList<>();
+				for (OrderProducts orderProduct : orderProductList) {
+					OrderDetails orderDetail = new OrderDetails();
+					orderDetail.setOrder(order);
+					orderDetail.setProduct(orderProduct.getProduct());
+					orderDetail.setProductCount(orderProduct.getProductQuantity());
+					orderDetailsList.add(orderDetail);
+				}
+				return orderDetailsList;
+			} else {
+				logger.info("Order doesn,t have any product associated with it.");
+			}
+
+			return Collections.emptyList();
+		} catch (Exception e) {
+			logger.error("Exception while setting invoice detials list for updation.", e);
+		}
+		return Collections.emptyList();
+	}
+
 	
 }
