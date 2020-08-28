f1f7b549ca1e56ff86c17498e70c5c7026ba97a1
NeerajSharma2020 <neeraj.sharma@successive.tech>
2020-08-28 15:30:35 +0530

    resolve conflicts

--- src/main/java/com/st/ims/utility/AppUtility.java
+++ src/main/java/com/st/ims/utility/AppUtility.java
@@ -10,125 +10,55 @@ import java.util.Random;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
-
-import com.st.ims.model.Invoice;
-import com.st.ims.model.InvoiceDetails;
-import com.st.ims.model.InvoiceProducts;
 import com.st.ims.model.Order;
 import com.st.ims.model.OrderDetails;
 import com.st.ims.model.OrderProducts;
 
 @Service
 public class AppUtility {
-	
+
 	private static final Logger logger = Logger.getLogger(AppUtility.class);
 	private static final int PERCENTAGE_DENOMINATOR = 100;
-	
+
 	private AppUtility() {
-		
+
 	}
+
 	/*
-	 * This method will take LocalDateTime as date and will return after two
-	 * months date.
+	 * This method will take LocalDateTime as date and will return after two months
+	 * date.
 	 */
 	private static LocalDateTime getDueDate(LocalDateTime createdDate) {
-		if(createdDate != null) {
+		if (createdDate != null) {
 			return createdDate.plusMonths(2);
 		}
 		return createdDate;
-	
-		
+
 	}
 
-	/* This method will return alphanumeric invoice number. */
-	
+	/* This method will return alphanumeric order number. */
+
 	private static String getInvoiceNumber() throws NoSuchAlgorithmException {
 		Random rand;
 		int leftLimit = 48; // numeral '0'
 		int rightLimit = 122; // letter 'z'
 		int targetStringLength = 10;
-        rand = SecureRandom.getInstanceStrong();
- 
-		return rand.ints(leftLimit, rightLimit + 1)
-		  .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-		  .limit(targetStringLength)
-		  .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-		  .toString();
-		   
-	   
-	}
-	
-	/*
-	 * This method will take invoice amount and commission percentage and will return
-	 * commission amount.
-	 */
-	private static double getCommissionAmount(double invoiceAmount , double commissionPercentage) {
-		return invoiceAmount*(commissionPercentage/PERCENTAGE_DENOMINATOR);
-	}
-	
-	/* This method will set default values for invoice object. */
-	public static Invoice setDefaultValues(Invoice invoice) {
-		try {
-			invoice.setDueDate(getDueDate(invoice.getCreateDate()));
-			invoice.setCommissionAmount(getCommissionAmount(invoice.getInvoiceAmount(),invoice.getCommissionPercentage()));
-			invoice.setInvoiceNumber(getInvoiceNumber());
-			return invoice;
-		} catch (NoSuchAlgorithmException e) {
-			logger.error("Exception while configuring dueDate,invoiceNumber and commissionAmount.",e);
-		}
-		return invoice;
+		rand = SecureRandom.getInstanceStrong();
+
+		return rand.ints(leftLimit, rightLimit + 1).filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+				.limit(targetStringLength)
+				.collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+
 	}
 
-	/* This method will create a new InvoiceDetails List, so that we can persist invoice object with this list */
-	public static List<InvoiceDetails> getInvoiceDetailsList(Invoice invoice, List<InvoiceProducts> invoiceProductList){
-		List<InvoiceDetails> invoiceDetailsList = null;
-		try {
-			Invoice filledInvoice = setDefaultValues(invoice);
-			if(!invoiceProductList.isEmpty()) {
-				 invoiceDetailsList = new ArrayList<>();
-				for(InvoiceProducts invoiceProduct:invoiceProductList){
-					InvoiceDetails newInvoiceDetail = new InvoiceDetails();
-					newInvoiceDetail.setInvoice(filledInvoice);
-					newInvoiceDetail.setProduct(invoiceProduct.getProduct());
-					newInvoiceDetail.setProductCount(invoiceProduct.getProductQuantity());
-					invoiceDetailsList.add(newInvoiceDetail);
-				}
-				return invoiceDetailsList;
-			}else {
-				logger.info("Invoice doesn,t have any product associated with it.");
-			}
-			    return Collections.emptyList();
-		} catch (Exception e) {
-			logger.error("Exception while getting InvoiceDetailsList.",e);
-		}
-		return Collections.emptyList();
+	/*
+	 * This method will take order amount and commission percentage and will return
+	 * commission amount.
+	 */
+	private static double getCommissionAmount(double invoiceAmount, double commissionPercentage) {
+		return invoiceAmount * (commissionPercentage / PERCENTAGE_DENOMINATOR);
 	}
-	
-	/* This method will create new InvoiceDetails list for update operation. */
-	public static List<InvoiceDetails> getUpdatedInvoiceList(Invoice invoice,List<InvoiceProducts> invoiceProductList){
-	
-		List<InvoiceDetails> invoiceDetailsList = null;
-		try {
-			if(!invoiceProductList.isEmpty()) {
-				invoiceDetailsList = new ArrayList<>();
-			for(InvoiceProducts invoiceProduct:invoiceProductList) {
-				 InvoiceDetails invoiceDetail = new InvoiceDetails();
-				 invoiceDetail.setInvoice(invoice);
-				 invoiceDetail.setProduct(invoiceProduct.getProduct());
-				 invoiceDetail.setProductCount(invoiceProduct.getProductQuantity());
-				 invoiceDetailsList.add(invoiceDetail);
-				 }
-			return invoiceDetailsList;
-			}else {
-				logger.info("Invoice doesn,t have any product associated with it.");
-			}
-			 return Collections.emptyList();
-		}catch(Exception e) {
-			logger.error("Exception while setting invoice detials list for updation.",e);
-		}
-		return Collections.emptyList();
-	}
-	
+
 	/*
 	 * This method will set default values for order such as orderAmount,
 	 * orderNumber and dueDate.
@@ -194,5 +124,4 @@ public class AppUtility {
 		return Collections.emptyList();
 	}
 
-	
 }
