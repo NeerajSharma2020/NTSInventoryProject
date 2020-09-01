ea954dfe130586de53f0317baf72475a6692a79f
NeerajSharma2020 <neeraj.sharma@successive.tech>
2020-09-01 17:50:00 +0530

    delete product data to avoid conflicts in future

--- src/main/java/com/st/ims/repo/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.st.ims.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.st.ims.model.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer>{
-
-}
--- src/main/java/com/st/ims/repo/ProductCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.st.ims.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.st.ims.model.ProductCategory;
-
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
-
-}
